23cb4207b08264c69c4590296194fc5e96d459f4
lightless <lightless@foxmail.com>
2016-07-03 14:12:55 +0800

    改进线程池，支持传递参数的多线程调用

--- utils/ThreadPool.py
+++ utils/ThreadPool.py
@@ -19,17 +19,21 @@ class ThreadPool:
 
     def add_function_list(self, function_list=[]):
         for fn in function_list:
-            self.add_function(fn)
+            self.add_function(fn[0], **fn[1])
 
-    def add_function(self, func):
+    def add_function(self, func, **kwargs):
         if callable(func):
-            self.__function_list.put(func)
+            self.__function_list.put((func, kwargs))
 
     def run(self):
         # 从队列中获取工作函数
         while not self.__function_list.empty():
-            fn = self.__function_list.get(block=True, timeout=1)
-            t = threading.Thread(target=fn, name=str(fn.im_class).split(".")[-1])
+            fn = self.__function_list.get_nowait()
+            try:
+                thread_name = str(fn[0].im_class).split(".")[-1]
+            except AttributeError:
+                thread_name = fn[0].__name__
+            t = threading.Thread(target=fn[0], name=thread_name, kwargs=fn[1])
             self.__thread_list.append(t)
 
         # 开始多线程运行工作函数
