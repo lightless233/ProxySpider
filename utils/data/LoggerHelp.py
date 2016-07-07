86fdeda999b92fc945c3ac6485ebdfa29e183cae
lightless <lightless@foxmail.com>
2016-07-07 17:24:37 +0800

    Add config file.

--- utils/data/LoggerHelp.py
+++ utils/data/LoggerHelp.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 import logging
+import ConfigParser
 
 __author__ = "lightless"
 __email__ = "root@lightless.me"
 
 
-console = logging.StreamHandler()
 formatter = logging.Formatter("[%(levelname)s] [(%(threadName)s)] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", "%H:%M:%S")
+
+console = logging.StreamHandler()
 console.setFormatter(formatter)
 
 logger = logging.getLogger("ProxySpiderLogger")
 logger.addHandler(console)
-logger.setLevel(logging.DEBUG)
+
+cf = ConfigParser.ConfigParser()
+cf.read("config.ini")
+if cf.has_option("ProxySpider", "debug") and cf.get("ProxySpider", "debug") == "On":
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
